81b2aa4050206050c02f2221a9de5debaf350740
Myniv <122717705+Myniv@users.noreply.github.com>
2025-08-08 01:47:05 +0700

    Create attendance history

---
+++ lib/screens/attendance_history_screen.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+import 'package:hr_attendance_tracker/widgets/custom_appbar.dart';
+
+class AttendanceHistoryScreen extends StatefulWidget {
+  final Color color = Color(0xFF1F1301);
+  // const AttendanceHistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AttendanceHistoryScreen> createState() =>
+      _AttendanceHistoryScreenState();
+}
+
+class _AttendanceHistoryScreenState extends State<AttendanceHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1F1301),
+      appBar: CustomAppbar(
+        title: "Attendance History",
+        onBack: () {
+          Navigator.pop(context);
+        },
+        icon: Icons.arrow_back,
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _selectDate(),
+              _summaryCard(),
+              ListView.builder(
+                itemCount: 20,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  // return Card(
+                  //   color: Color(0xFF1F1301),
+                  //   shadowColor: Colors.white,
+                  //   child: _attendanceCard("Friday", "7", "22:00", "23:00"),
+                  // );
+                  return _attendanceCard("Friday", "7", "22:00", "23:00");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectDate() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: InkWell(
+        onTap: () {
+          print("object");
+        },
+        child: Container(
+          height: 50,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xFFE8D5C4),
+            border: Border.all(color: Color(0xFFD4A574), width: 2),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.calendar_month, color: Colors.black, size: 28),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "October 2025",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              ),
+              Icon(Icons.arrow_drop_down, color: Colors.black, size: 28),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _summaryCard() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10),
+      child: Container(
+        height: 230,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Color(0xFFA0522D),
+          border: Border.all(color: Color(0xFFD4A574), width: 2),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _summaryItem("Absent", "13"),
+                _summaryItem("Late Check In", "3"),
+                _summaryItem("Early Clock In", "6"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _summaryItem("Present", "18"),
+                _summaryItem("On Time", "15"),
+                _summaryItem("Overtime", "2"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _summaryItem(String title, String value) {
+    return Expanded(
+      child: Column(
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFFE8D5C4),
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+            decoration: BoxDecoration(
+              color: Color(0xFFE8D5C4),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              value,
+              style: TextStyle(
+                color: Color(0xFF5D2E0A),
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _attendanceCard(
+    String day,
+    String todayDate,
+    String checkIn,
+    String checkOut,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10),
+      child: Container(
+        height: 120,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: Color(0xFFFFA800)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFA800),
+                  border: Border.all(color: Color(0xFFFFA800)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      day,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      todayDate,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                // decoration: BoxDecoration(
+                //   color: Color.fromARGB(255, 255, 255, 255),
+                // ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Check In",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      checkIn,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                // decoration: BoxDecoration(
+                //   color: Color.fromARGB(255, 255, 255, 255),
+                // ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Check Out",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      checkIn,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
