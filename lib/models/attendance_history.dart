b21dcea5d273305218f7643f14b6210b9898cf3c
Myniv <122717705+Myniv@users.noreply.github.com>
2025-08-08 07:29:22 +0700

    Create attendance_history entity

---
+++ lib/models/attendance_history.dart
@@ -0,0 +1,13 @@
+class AttendanceHistory {
+  final String day;
+  final String date;
+  final String inTime;
+  final String outTime;
+
+  AttendanceHistory({
+    required this.day,
+    required this.date,
+    required this.inTime,
+    required this.outTime,
+  });
+}
