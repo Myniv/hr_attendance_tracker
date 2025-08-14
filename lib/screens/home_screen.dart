f5bf0c24efa4b539b6643ba21d0887288e7661f2
Myniv <122717705+Myniv@users.noreply.github.com>
2025-08-14 23:19:16 +0700

    Create card for clockin and clockout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,45 +1,158 @@
 import 'package:flutter/material.dart';
 import 'package:hr_attendance_tracker/custom_theme.dart';
+import 'package:hr_attendance_tracker/models/attendance_history.dart';
+import 'package:hr_attendance_tracker/models/profile.dart';
 import 'package:hr_attendance_tracker/screens/attendance_history_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  // const HomeScreen({Key? key}) : super(key: key);
+
+  DateTime today = DateTime.now();
+  var dayName = CustomTheme().formatDay(DateTime.now());
+  var monthName = CustomTheme().formatMonth(DateTime.now());
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      alignment: Alignment.center,
-      decoration: BoxDecoration(color: CustomTheme.backgroundScreenColor),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Home Screen',
-            style: CustomTheme().largeFont(CustomTheme.whiteButNot),
-          ),
-          SizedBox(
-            height: 100,
-            width: 500,
-            child: Text(
-              'An HR attendance tracker is a tool used to monitor and record employee attendance, including working hours, absences, and leave. It helps HR departments ensure accurate payroll, improve productivity, and maintain compliance with company policies. By using an attendance tracker, organizations can streamline workforce management and make informed decisions based on real-time attendance data.',
-              style: CustomTheme().superSmallFont(
-                CustomTheme.whiteButNot,
-                FontWeight.normal,
+    final hour = today.hour % 12 == 0 ? 12 : today.hour % 12;
+    final minute = today.minute.toString().padLeft(2, '0');
+    final period = today.hour >= 12 ? 'PM' : 'AM';
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hi, ${Profile().name} 👋',
+                      style: CustomTheme().largeFont(
+                        Colors.white,
+                        FontWeight.normal,
+                        context,
+                      ),
+                    ),
+                    Text(
+                      "Welcome Back!",
+                      style: CustomTheme().largeFont(
+                        Colors.white,
+                        FontWeight.bold,
+                        context,
+                      ),
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage('assets/images/profile.png'),
+                ),
+              ],
+            ),
+            SizedBox(height: 32),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: CustomTheme.colorYellow,
+                border: Border.all(color: CustomTheme.colorGold, width: 2),
+                borderRadius: CustomTheme.borderRadius,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "$dayName, ${today.day} $monthName",
+                    style: CustomTheme().smallFont(
+                      CustomTheme.colorBrown,
+                      null,
+                      context,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "$hour:$minute",
+                        style: CustomTheme().superLargeFont(
+                          CustomTheme.colorBrown,
+                          null,
+                          context,
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        period,
+                        style: CustomTheme().superLargeFont(
+                          CustomTheme.colorBrown,
+                          FontWeight.normal,
+                          context,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 6),
+
+                  Text(
+                    "Not Clock In Yet",
+                    style: CustomTheme().superSmallFont(
+                      CustomTheme.colorBrown,
+                      FontWeight.normal,
+                      context,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: ElevatedButton.icon(
+                      icon: Icon(Icons.input, color: CustomTheme.colorBrown),
+                      label: Text(
+                        'Clock In',
+                        style: CustomTheme().smallFont(
+                          CustomTheme.colorBrown,
+                          null,
+                          context,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(
+                            color: CustomTheme.colorGold,
+                            width: 2,
+                          ),
+                        ),
+                        elevation: 3,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AttendanceHistoryScreen(),
-                ),
-              );
-            },
-            child: Text('History Attendance'),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AttendanceHistoryScreen(),
+                  ),
+                );
+              },
+              child: Text('History Attendance'),
+            ),
+          ],
+        ),
       ),
     );
   }
