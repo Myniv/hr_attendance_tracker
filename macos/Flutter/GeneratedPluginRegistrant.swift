928751f64edd0ce4785435d02a38c9929fb34f9a
Myniv <122717705+Myniv@users.noreply.github.com>
2025-08-29 08:54:02 +0700

    install animatedItem plugin

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import animated_item
 import file_selector_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AnimatedItemPlugin.register(with: registry.registrar(forPlugin: "AnimatedItemPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
 }
