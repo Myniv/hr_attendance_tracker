da3d69eb166e175de6b45d255f37f72545fd1b05
Myniv <122717705+Myniv@users.noreply.github.com>
2025-09-04 14:46:14 +0700

    Install plugin shared_preferences and http

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import animated_item
 import file_selector_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AnimatedItemPlugin.register(with: registry.registrar(forPlugin: "AnimatedItemPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
